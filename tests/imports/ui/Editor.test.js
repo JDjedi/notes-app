d92e2d84d8fde23be9e0377ed68124e06e165935
JDjedi <jcdiaz1201@gmail.com>
2019-05-18 19:59:17 -0500

    more editor test cases added

--- tests/imports/ui/Editor.test.js
+++ tests/imports/ui/Editor.test.js
@@ -38,7 +38,45 @@ if (Meteor.isClient) {
 			wrapper.find('button').simulate('click');
 			expect(history.push).toHaveBeenCalledWith('/dashboard');
 			expect(call).toHaveBeenCalledWith('notes.remove', notes[0]._id);
-		})
+		});
+
+		it('Should update the note body on textarea change', function() {
+			const newBody = "this is my new body text"
+			const wrapper = mount(<Editor history={history} call={call} selectedNoteId={notes[0]} note={notes[0]}/>)
+			wrapper.find('textarea').simulate('change', {
+				target: {
+					value: newBody
+				}
+			});
+
+			expect(wrapper.state('body')).toBe(newBody);
+			expect(call).toHaveBeenCalledWith('notes.update', notes[0]._id, {body: newBody})
+		});
+
+		it('Should update the note title on change', function() {
+			const newTitle = "this is my new title text"
+			const wrapper = mount(<Editor history={history} call={call} selectedNoteId={notes[0]} note={notes[0]}/>)
+			wrapper.find('input').simulate('change', {
+				target: {
+					value: newTitle
+				}
+			});
+
+			expect(wrapper.state('title')).toBe(newTitle);
+			expect(call).toHaveBeenCalledWith('notes.update', notes[0]._id, {title: newTitle})
+		});
+
+		it('Should setState for new note', function() {
+			const wrapper = mount(<Editor history={history} call={call}/>)
+			wrapper.setProps({ // only available in enzyme, not in reg components
+				selectedNoteId: notes[0]._id,
+				note: notes[0]
+			});
+
+			expect(wrapper.state('title')).toBe(notes[0].title)
+			expect(wrapper.state('body')).toBe(notes[0].body)
+		});
+
 	});
 }
 
